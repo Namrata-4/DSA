ee7274a2446fa93c0bbe0f74910a2ac55ef2475f
Namrata Bera <benamrata@gmail.com>
2026-04-02 21:07:23 +0530

    leet-array=problem

--- leetArray/bestTimeToBuyAndSellStock.cpp
+++ leetArray/bestTimeToBuyAndSellStock.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+int maxProfit(vector<int>& prices){
+    int minPrice=INT_MAX;
+    int maxProfit=0;
+    for(int price:prices){
+        minPrice=min(minPrice,price);
+        maxProfit=max(maxProfit,price-minPrice);
+    }
+    cout<< maxProfit;
+    return 0;
+};
 int main(){
-    
-}
+    vector<int>prices={7,1,5,3,6,4};
+    cout<<"resut :"<<endl;
+    maxProfit(prices);
+    return 0;
+};
